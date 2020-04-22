e0868558c666bc9410e036b2b4671a4e2589ffbe
hencan <49315755+hencan@users.noreply.github.com>
2020-04-22 01:24:39 -0300

    Ver 0.05
    Diversas revisões

--- calcRecibos/scripts/script.js
+++ calcRecibos/scripts/script.js
@@ -1,24 +1,39 @@
 var keys = document.querySelectorAll('#keyboard div')
 var hist = document.querySelectorAll('#histText, #histText span, #histTitle')
+var commaOp = 0
 var posOp = 0
+var inputMem = ""
+var inputVal = ""
 
 for (var i = 1; i < keys.length; i++) {
     keys[i].onclick = function () {
-        var btnVal
-        var input = document.querySelector('#display')
-        var inputVal = input.innerHTML
+        var btnVal = ""
+        var inputNum = document.querySelector('#displayNum')
+        var inputSub = document.querySelector('#displaySub')
+        
         btnVal = this.innerHTML
 
         if (btnVal == 'C') {
-            input.innerHTML = 0
+            inputNum.innerHTML = 0
+            inputSub.innerHTML = 0
+            inputMem = 0
+            inputVal = 0
         } else if (btnVal == '=') {
-                inputVal = eval(input.innerHTML)
+                console.log(inputNum.innerHTML)
+                inputMem += inputNum.innerHTML
+                console.log("InputMem Inicial: " + inputMem) 
+                inputVal = eval(inputMem)
+                inputMem = inputVal
+                console.log(inputMem) 
+                console.log(inputVal)
+                inputSub.innerHTML += inputNum.innerHTML
                 document.querySelector('#histText').innerHTML += "<br><span>✎</span>"
-                document.querySelector('#histCalc').innerHTML += "<br>" + input.innerHTML + " = " + inputVal
-                input.innerHTML = inputVal
+                document.querySelector('#histCalc').innerHTML += "<br>" + inputSub.innerHTML + " = " + inputVal
+                inputNum.innerHTML = inputVal
                 posOp=1
 
-                hist = document.querySelectorAll('#histText, #histText span, #histTitle')
+
+                hist = document.querySelectorAll('#histText span, #histTitle')
 
                 for (var i = 0; i < hist.length; i++) {
                     hist[i].onclick = function () {
@@ -27,53 +42,72 @@ for (var i = 1; i < keys.length; i++) {
 
                         if (btnHistVal[0] == '✎') {
                             this.innerHTML = "✎ " + prompt("Insira seu texto personalido")
-
                         }
                     }
                 }
             } else if (btnVal == '⌫') {
-                    if (input.innerHTML == "") {
-                        input.innerHTML = 0
-                    } else if (input.innerHTML.length < 2) {
-                        input.innerHTML = 0
+                    if (inputNum.innerHTML == "") {
+                        inputNum.innerHTML = 0
+                    } else if (inputNum.innerHTML.length < 2) {
+                        inputNum.innerHTML = 0
                     } else {
-                        input.innerHTML = input.innerHTML.substring(0,(input.innerHTML.length - 1))
+                        inputNum.innerHTML = inputNum.innerHTML.substring(0,(inputNum.innerHTML.length - 1))
                         posOp = 0
                     }
                 } else if (btnVal == '⁺/₋') {
-                        if (input.innerHTML == "") {
-                            input.innerHTML = 0
-                        } else if (input.innerHTML == 0) {
-                            input.innerHTML = 0
+                        if (inputNum.innerHTML == "") {
+                            inputNum.innerHTML = 0
+                        } else if (inputNum.innerHTML == 0) {
+                            inputNum.innerHTML = 0
                         } else {
-                            input.innerHTML = eval(input.innerHTML) * (-1) 
+                            inputNum.innerHTML = eval(inputNum.innerHTML) * (-1) 
                         }
-                    // } else if (btnVal == '.') {
-                    //     if (input.innerHTML == "") {
-                    //         input.innerHTML = 0
-                    //     } else if (input.innerHTML == 0) {
-                    //         input.innerHTML = 0
-                    //     } else if ({
-                    //         input.innerHTML = eval(input.innerHTML) * (-1) 
-                    //     }
+                    } else if (btnVal == '÷'){
+                        if (inputSub.innerHTML == "0") {
+                            inputSub.innerHTML = inputNum.innerHTML + btnVal
+                            inputMem = inputNum.innerHTML + btnVal
+                            inputMem = inputMem.replace("÷", "/")
+                            posOp=1
+                        } else {
+                            inputSub.innerHTML = inputNum.innerHTML + btnVal
+                            inputMem = inputNum.innerHTML + btnVal
+                            inputMem = inputMem.replace("÷", "/")
+                            posOp=1
+                        }
+                    } else if (btnVal == '×'){
+                        if (inputSub.innerHTML == "0") {
+                            inputSub.innerHTML = inputNum.innerHTML + btnVal
+                            inputMem = inputNum.innerHTML + btnVal
+                            inputMem = inputMem.replace("×", "*")
+                            posOp=1
+                        } else {
+                            inputSub.innerHTML = inputNum.innerHTML + btnVal
+                            inputMem = inputNum.innerHTML + btnVal
+                            inputMem = inputMem.replace("×", "*")
+                            posOp=1
+                        }
+                    } else if (btnVal == ','){
+                        inputNum.innerHTML += btnVal
+                        commaOp = 1
+                    } else if (btnVal == '+'){
+                        inputSub.innerHTML = inputNum.innerHTML + btnVal
+                        inputMem = inputNum.innerHTML + btnVal
+                        posOp=1
+                    } else if (btnVal == '-'){
+                        inputSub.innerHTML = inputNum.innerHTML + btnVal
+                        inputMem = inputNum.innerHTML + btnVal
+                        posOp=1
                     } else if (posOp > 0) {
-                        input.innerHTML = ''
-                        input.innerHTML += btnVal
+                        inputNum.innerHTML = ''
+                        inputNum.innerHTML += btnVal
                         posOp=0
                     } else {
-                        if (input.innerHTML == 0)  {
-                            input.innerHTML = ''
-                            input.innerHTML += btnVal
+                        if (inputNum.innerHTML == 0)  {
+                            inputNum.innerHTML = ''
+                            inputNum.innerHTML += btnVal
                         } else {
-                            input.innerHTML += btnVal
+                            inputNum.innerHTML += btnVal
                         }
-                    }
-                    
-                            
-                                      
-                
-                
-            
-        
+                    }   
     }
 }
