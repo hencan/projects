e41fd93418fc056ec930fb01af4746e831183ae8
hencan <49315755+hencan@users.noreply.github.com>
2020-04-23 13:55:26 -0300

    Ver 0.11
    Nova versão de UI

--- calcRecibos/oldVersions/ver 0.10 [estavel]/scripts/support.js
+++ calcRecibos/oldVersions/ver 0.10 [estavel]/scripts/support.js
@@ -1,32 +1,15 @@
 var openMenu = 0
-var openHistCalc = 0
 var openPreview = 0
 var openShare = 0
 
-function openCloseMenuToggle() {
+function openClose() {
     if (openMenu == 1) {
-        document.querySelector(".sidebarMenu").style.right = -60 + "px"
+        document.querySelector(".sidebarMenu").style.right = -155 + "px"
         document.querySelector(".menu-background").style.right = -10000 + "px"
         openMenu = 0
     } else {
         document.querySelector(".sidebarMenu").style.right = 0
         document.querySelector(".menu-background").style.right = 0
-        openMenu = 1
-    }
-}
-
-function openCloseHistCalc() {
-    if (openMenu == 1) {
-        document.querySelector("#historic").style.display = "none"
-        document.querySelector("#calculator").style.display = "block"
-        document.querySelector("#openCloseHist").style.display = "block"
-        document.querySelector("#openCloseCalc").style.display = "none"
-        openMenu = 0
-    } else {
-        document.querySelector("#historic").style.display = "block"
-        document.querySelector("#calculator").style.display = "none"
-        document.querySelector("#openCloseHist").style.display = "none"
-        document.querySelector("#openCloseCalc").style.display = "block"
         openMenu = 1
     }
 }
