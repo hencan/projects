65e8d71e16d19f83e3de3c502de492c038db7f18
hencan <49315755+hencan@users.noreply.github.com>
2020-07-17 20:07:53 -0300

    update

--- portifolio/service-worker.js
+++ portifolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portifolio/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js"
+  "/projects/portifolio/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/portifolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
