37bc848415a8e1545b5f749abcc3dfe079a5c553
Henrique Candolo <hen.can@hotmail.com>
2020-11-21 11:17:20 -0300

    Atualização pequenas
    Mudança de formEmail.html para index.html
    Inclusão de comentários nos arquivos

--- formEmail/script.js
+++ formEmail/script.js
@@ -1,24 +1,27 @@
+// Função de Valiidação do Campo de Email do Formulário
 function validacaoEmail(field) {
-    usuario = field.value.substring(0, field.value.indexOf("@"));
-    dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
+    user = field.value.substring(0, field.value.indexOf("@")); // Trecho até o @
+    domain = field.value.substring(field.value.indexOf("@") + 1, field.value.length); //Trecho após o @
+    console.log(user)
+    console.log(domain)
 
-    if ((usuario.length >= 1) &&
-        (dominio.length >= 3) &&
-        (usuario.search("@") == -1) &&
-        (dominio.search("@") == -1) &&
-        (usuario.search(" ") == -1) &&
-        (dominio.search(" ") == -1) &&
-        (dominio.search(".") != -1) &&
-        (dominio.indexOf(".") >= 1) &&
-        (dominio.lastIndexOf(".") < dominio.length - 1)) {
-        if (dominio == "gmail.com" || dominio == "hotmail.com" || dominio == "outlook.com" || dominio == "outlook.com.br") {
+    if ((user.length >= 1) && //Verificação de tamanho mínimo
+        (domain.length >= 3) && //Verificação de tamanho mínimo
+        (user.search("@") == -1) && //Confirma
+        (domain.search("@") == -1) &&
+        (user.search(" ") == -1) &&
+        (domain.search(" ") == -1) &&
+        (domain.search(".") != -1) &&
+        (domain.indexOf(".") >= 1) &&
+        (domain.lastIndexOf(".") < domain.length - 1)) {
+        if (domain == "gmail.com" || domain == "hotmail.com" || domain == "outlook.com" || domain == "outlook.com.br") {
             document.getElementById("msgEmail").innerHTML = "<font color='blue'>E-mail válido";
             document.getElementById("passField").disabled = false;
             document.getElementById("passField").focus();
             document.getElementById("subBtn").disabled = false;
         }
         else {
-            document.getElementById("msgEmail").innerHTML = '"@' + dominio + '" não aceito.';
+            document.getElementById("msgEmail").innerHTML = '"@' + domain + '" não aceito.';
             document.getElementById("subBtn").disabled = true;
             document.getElementById("emailField").focus();
         }
