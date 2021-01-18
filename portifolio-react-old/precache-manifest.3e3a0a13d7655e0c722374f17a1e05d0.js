16a01076c9e50904ba7d45a9140e91fc5ad6387c
Henrique Candolo <hen.can@hotmail.com>
2021-01-18 19:06:49 -0300

    update html reference

--- portifolio-react-old/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js
+++ portifolio-react-old/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js
@@ -1,106 +1,106 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4275f1d729839b71900a2767164222c5",
-    "url": "/projects/portifolio-react-old/index.html"
+    "url": "/projects-old/portifolio-react-old/index.html"
   },
   {
     "revision": "400b54c73cb37d5bbbc1",
-    "url": "/projects/portifolio-react-old/static/css/main.8ceca23e.chunk.css"
+    "url": "/projects-old/portifolio-react-old/static/css/main.8ceca23e.chunk.css"
   },
   {
     "revision": "d6cc01f12e3b9f87abd1",
-    "url": "/projects/portifolio-react-old/static/js/2.4e5dc771.chunk.js"
+    "url": "/projects-old/portifolio-react-old/static/js/2.4e5dc771.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/projects/portifolio-react-old/static/js/2.4e5dc771.chunk.js.LICENSE.txt"
+    "url": "/projects-old/portifolio-react-old/static/js/2.4e5dc771.chunk.js.LICENSE.txt"
   },
   {
     "revision": "400b54c73cb37d5bbbc1",
-    "url": "/projects/portifolio-react-old/static/js/main.d8324d55.chunk.js"
+    "url": "/projects-old/portifolio-react-old/static/js/main.d8324d55.chunk.js"
   },
   {
     "revision": "d40965c8a36843967f67",
-    "url": "/projects/portifolio-react-old/static/js/runtime-main.af465e72.js"
+    "url": "/projects-old/portifolio-react-old/static/js/runtime-main.af465e72.js"
   },
   {
     "revision": "0ef6b70e15446bb9daded1c786482961",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen0.0ef6b70e.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen0.0ef6b70e.png"
   },
   {
     "revision": "3dc2971e43e3e44207271a7a887071f1",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen1.3dc2971e.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen1.3dc2971e.png"
   },
   {
     "revision": "9ef567e8a8bebc2ab5ccf7c916322ee0",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen2.9ef567e8.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen2.9ef567e8.png"
   },
   {
     "revision": "50f60153395ef24a02754cd4c34e21ea",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen3.50f60153.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen3.50f60153.png"
   },
   {
     "revision": "4da0b531fcc7c26cdaa464eafd6999b3",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen4.4da0b531.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen4.4da0b531.png"
   },
   {
     "revision": "519570dd1e33339b3e6ec3c468d842ff",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen5.519570dd.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen5.519570dd.png"
   },
   {
     "revision": "02f6cecfa98dc65f2ad999921a4a1b3d",
-    "url": "/projects/portifolio-react-old/static/media/commentCalcScreen6.02f6cecf.png"
+    "url": "/projects-old/portifolio-react-old/static/media/commentCalcScreen6.02f6cecf.png"
   },
   {
     "revision": "4fdd1caecb806babdc07e8f78c98c0ef",
-    "url": "/projects/portifolio-react-old/static/media/fotoHenrique2.4fdd1cae.jpg"
+    "url": "/projects-old/portifolio-react-old/static/media/fotoHenrique2.4fdd1cae.jpg"
   },
   {
     "revision": "094b99478c69475dcb79fbbc6e47eee6",
-    "url": "/projects/portifolio-react-old/static/media/level00.094b9947.gif"
+    "url": "/projects-old/portifolio-react-old/static/media/level00.094b9947.gif"
   },
   {
     "revision": "35336cbd885a38b5372db329777a8dae",
-    "url": "/projects/portifolio-react-old/static/media/level01.35336cbd.gif"
+    "url": "/projects-old/portifolio-react-old/static/media/level01.35336cbd.gif"
   },
   {
     "revision": "e77790e21c01b223a2b517dd83f38b75",
-    "url": "/projects/portifolio-react-old/static/media/level02.e77790e2.gif"
+    "url": "/projects-old/portifolio-react-old/static/media/level02.e77790e2.gif"
   },
   {
     "revision": "e13ee066c5b530b813119d5c291693fa",
-    "url": "/projects/portifolio-react-old/static/media/level03.e13ee066.gif"
+    "url": "/projects-old/portifolio-react-old/static/media/level03.e13ee066.gif"
   },
   {
     "revision": "33828a4a135eedc3bb73dcc2930e1295",
-    "url": "/projects/portifolio-react-old/static/media/template00.33828a4a.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template00.33828a4a.png"
   },
   {
     "revision": "7a450f2ef104f189d0376b8eeb485dff",
-    "url": "/projects/portifolio-react-old/static/media/template01.7a450f2e.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template01.7a450f2e.png"
   },
   {
     "revision": "2bb24f650f0d61ad19d22f0f9ddf8e50",
-    "url": "/projects/portifolio-react-old/static/media/template02.2bb24f65.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template02.2bb24f65.png"
   },
   {
     "revision": "37cd97fab302d76a2e1c55492a281d5b",
-    "url": "/projects/portifolio-react-old/static/media/template03.37cd97fa.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template03.37cd97fa.png"
   },
   {
     "revision": "c5210befcf823f960cec812213128481",
-    "url": "/projects/portifolio-react-old/static/media/template04.c5210bef.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template04.c5210bef.png"
   },
   {
     "revision": "b1ff3426caad8a2e60e5176bd15fc6e3",
-    "url": "/projects/portifolio-react-old/static/media/template05.b1ff3426.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template05.b1ff3426.png"
   },
   {
     "revision": "099bdeca13498ae4484f64274dc3cd44",
-    "url": "/projects/portifolio-react-old/static/media/template06.099bdeca.png"
+    "url": "/projects-old/portifolio-react-old/static/media/template06.099bdeca.png"
   },
   {
     "revision": "52f911325ba42f7b170d0f0886f05e8b",
-    "url": "/projects/portifolio-react-old/static/media/vuejsccalcScreen0.52f91132.png"
+    "url": "/projects-old/portifolio-react-old/static/media/vuejsccalcScreen0.52f91132.png"
   }
 ]);
--- portifolio-react-old/service-worker.js
+++ portifolio-react-old/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/portifolio-react-old/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js"
+  "/projects-old/portifolio-react-old/precache-manifest.3e3a0a13d7655e0c722374f17a1e05d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/portifolio-react-old/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects-old/portifolio-react-old/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
